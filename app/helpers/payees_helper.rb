e33a0bebe1fc7acd73a3a2566c8fc3abbad56fb5
Jonathan Pares <jonathan.pares@gmail.com>
2015-12-08 17:26:47 +0100

    Handle in views when the sign-up url failed to be retrieved.

--- app/helpers/payees_helper.rb
+++ app/helpers/payees_helper.rb
@@ -2,8 +2,10 @@ module PayeesHelper
   def signed?(payee)
     if payee.signed
       'Signed'
-    else
+    elsif payee.sign_up_url.present?
       link_to('Payoneer sign up', payee.sign_up_url)
+    else
+      'No link ...'
     end
   end
 end
